618d190f19b03ee25fc06a0072de217c0180d6da
Hrithik-C-A <hrithikca@gmail.com>
2023-03-31 10:53:55 +0530

    added login feature

---
+++ controllers/loginUser.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcrypt')
+const User = require('../models/User')
+
+module.exports = (req,res)=>{
+    const {username,password} = req.body
+
+    User.findOne({username: username})
+    .then((user) => {
+        if (user) {
+
+          bcrypt.compare(password, user.password, (err, same) => {
+            if (same) {
+              res.redirect('/')
+
+            } else {
+              res.redirect('/auth/login')
+            }
+          })
+        } else {
+          res.redirect('/auth/login')
+        }
+      })
+  }
+
+  
+  
+  
+  
+  
+  
--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const storePostController = require('./controllers/storePost')
 const validateMiddleware = require('./middlewares/validateMiddleware')
 const newUserController = require('./controllers/newUser')
 const storeUserController = require('./controllers/storeUser')
+const loginController = require('./controllers/login')
+const loginUserController = require('./controllers/loginUser')
 
 mongoose.connect('mongodb://0.0.0.0:27017/blog',{useNewUrlParser: true})
 // mongoose.connection.collection('blogposts').insertOne({title:'i dont know'})
@@ -31,8 +33,13 @@ app.get('/auth/register',newUserController)
 
 app.get('/posts/new',newPostController
 )
+app.get('/auth/login',loginController)
+
 app.post('/posts/store',storePostController)
 
 app.post('/users/register',storeUserController)
+
+app.post('/users/login',loginUserController)
+
 
 app.listen(4000, ()=>{ console.log('App listening on port 4000') }) 
