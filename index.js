6fbefdf029c214f69a149aec572f8bf92b3dc2d1
Hrithik-C-A <hrithikca@gmail.com>
2023-04-01 10:34:33 +0530

    authentication updated

--- index.js
+++ index.js
@@ -4,18 +4,20 @@ const ejs = require('ejs')
 const fileUpload = require('express-fileupload')
 const expressSession = require('express-session')
 const mongoose = require('mongoose')
+
+const validateMiddleware = require('./middlewares/validateMiddleware')
+const authMiddleware = require('./middlewares/authMiddleware')
+const redirectIfAuthenticatedMiddleware = require('./middlewares/redirectIfAuthenticatedMiddleware')
+
 const newPostController = require('./controllers/newPost')
 const homeController = require('./controllers/home')
 const getPostController = require('./controllers/getPost')
 const storePostController = require('./controllers/storePost')
-
-const validateMiddleware = require('./middlewares/validateMiddleware')
-const authMiddleware = require('./middlewares/authMiddleware')
-
 const newUserController = require('./controllers/newUser')
 const storeUserController = require('./controllers/storeUser')
 const loginController = require('./controllers/login')
 const loginUserController = require('./controllers/loginUser')
+const logoutController = require('./controllers/logout')
 
 mongoose.connect('mongodb://0.0.0.0:27017/blog',{useNewUrlParser: true})
 // mongoose.connection.collection('blogposts').insertOne({title:'i dont know'})
@@ -26,9 +28,13 @@ app.use(express.urlencoded({extended: true}))
 app.use(fileUpload())
 app.use(expressSession({
     secret: 'blog secret code'
-}))
-
+})) 
 app.use('/posts/store',validateMiddleware)
+global.loggedIn = null;
+app.use("*", (req, res, next) => {
+     loggedIn = req.session.userId; 
+     next()  
+     });
 
 app.set('view engine','ejs')
 
@@ -36,17 +42,22 @@ app.get('/', homeController)
 
 app.get('/post/:id',getPostController)
 
-app.get('/auth/register',newUserController)
+app.get('/auth/register',redirectIfAuthenticatedMiddleware,newUserController)
 
 app.get('/posts/new',authMiddleware,newPostController)
 
-app.get('/auth/login',loginController)
+app.get('/auth/login',redirectIfAuthenticatedMiddleware,loginController)
 
 app.post('/posts/store',authMiddleware,storePostController)
 
-app.post('/users/register',storeUserController)
+app.post('/users/register',redirectIfAuthenticatedMiddleware,storeUserController)
 
-app.post('/users/login',loginUserController)
+app.post('/users/login',redirectIfAuthenticatedMiddleware,loginUserController)
+
+app.get('/auth/logout',logoutController)
+
+app.use((req, res) => res.render('notfound'));
+
 
 
 app.listen(4000, ()=>{ console.log('App listening on port 4000') }) 
